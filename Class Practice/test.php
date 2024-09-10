0a95b8fad53e19148ac211809ba92121c31a9867
Md. Shekul Islam <shekul.bd201@gmail.com>
2024-09-10 08:34:08 +0600

    update

---
+++ Class Practice/test.php
@@ -0,0 +1,46 @@
+<?php
+echo "<h1>Hello World</h1>";
+echo phpversion()."<br>"; 
+?>
+
+<?php
+$color = "red";
+
+echo $color."<br><br>";
+
+Echo "hello world" ."<br>";
+
+
+ECHO "HELLO WORLD" ."<br>";
+
+$a=4;
+$b=3;
+$c=$a+$b;
+
+echo $c."<br>";
+
+define("CN","This is my first code");
+echo CN."<br>";
+
+
+
+
+
+$t=12;
+$p=4;
+$s=8;
+$k=$s/$p;
+printf($k+$t)."<br>";
+
+$name=array(
+    array("Rakib",120,"Mirpur"),
+    array("Shakil",130,"Kazipara"),
+    array("Raju",234,"Sciencelab"),
+);
+
+echo $name[1][2];
+echo "<br>";
+
+
+?>
+
---
+++ php_final_evidence/loging.php
@@ -0,0 +1,40 @@
+<?php session_start ();
+
+$_SESSION
+
+?>
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+<h3>Form Authentication</h3>
+
+
+
+<form action="#" method="post">
+    <div>
+        Username<br/>
+        <input type="text" name="txtUsername">
+    </div> <br/>
+
+    <div>
+        Password<br/>
+        <input type="text" name="txtPassword">
+    </div> 
+
+    <div>
+        <input type="submit" value="Log In" name="btnlogin">
+    </div>
+</form>
+</body>
+</html>
